6214b05af11d8af38677366b57e893c5cf218330
udeshika123cyber <udeshikaprabhashini21@gmail.com>
2025-06-12 19:33:05 +0530

    dart

--- my_test_project/bin/my_test_project.dart
+++ my_test_project/bin/my_test_project.dart
@@ -1,6 +1,13 @@
-import 'package:my_test_project/my_test_project.dart' as my_test_project;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${my_test_project.calculate()}!');
-  print('Total : ${my_test_project.add()}!!');
+main() {
+  stdout.write('Whats your name: ');
+  String name = stdin.readLineSync() ?? ' ';
+  // print('My name is $name');
+
+  stdout.write('Whats your age: ');
+  String age = stdin.readLineSync() ?? ' ';
+  // print('My age is $age');
+
+  print("My name is $name and i'm $age years old.");
 }
