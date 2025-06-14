792482f9c16bd7932c80b18303154ff8d2dc1d1a
udeshika123cyber <udeshikaprabhashini21@gmail.com>
2025-06-14 13:56:03 +0530

    modified

--- my_test_project/bin/my_test_project.dart
+++ my_test_project/bin/my_test_project.dart
@@ -12,13 +12,11 @@ import 'dart:io';
 //   print("My name is $name and i'm $age years old.");
 // }
 
-
 // For loop//
 // main() {
 // for (int i = 0; i < 10; i++) {
 //   print('Hello ${i + 1}');
 // }
-
 
 // If condition //
 //   int age = 20;
@@ -35,7 +33,6 @@ import 'dart:io';
 //   }
 // }
 
-
 // String interpolation //
 // int age() {
 //   return 20;
@@ -49,5 +46,50 @@ import 'dart:io';
 //   );
 // }
 
-
 // Const & Final //
+// class Animal {
+//   final String item = "Alphebet";
+//   final List items = ["pens", "cars", "buttons", "balls"];
+// }
+// main() {
+//   Animal rw = new Animal();
+//   print(rw.item); //print Alphebet//
+//   //rw.item = "Capital";  //Error ,Can't assign new value//
+//   rw.items.add("Desk");
+//   print(rw.items); //can add new value in to List//
+// }
+
+// main() {
+//   const double price = 23.5;
+//   const double weight = 450.50;
+//   const List item = ["slow", "medium", "fast"];
+//   print(price);
+//   print(weight);
+//   print(item);
+//   //price +=50; //Error - Can't modify
+//   //weight.add("faster"); // Error- Can't add to immutable list
+// }
+
+// class Location {
+//   final int x;
+//   final int y;
+//   const Location(this.x, this.y);
+// }
+
+// main() {
+// const Location gate = const Location(400, 200);
+// const Location tower = const Location(500, 200);
+// const Location tube = const Location(400, 200);
+
+// print(gate == tower); //false
+// print(gate == tube); //true
+
+//   Location gate = new Location(200, 400);
+//   Location tower = new Location(300, 400);
+//   Location tube = new Location(200, 400);
+//   print(tower == tube); //false
+//   print(gate == tube); //false
+// }
+
+// Operators//
+void main() {}
